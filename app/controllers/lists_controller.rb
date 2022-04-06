45863d915b09ef35dcd22c6b55d7cc7013ace504
EC2 Default User <ec2-user@ip-172-31-83-11.ec2.internal>
2022-04-06 03:48:24 +0000

    ２章まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,21 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
-
+  
+  def create
+    
+    #データを受け取り新規登録するためのインスタンス作成
+    list=List.new(list_params)
+    
+    #データをデータベースに保存するためのsaveメソッド
+    list.save
+    #トップ画面に戻る
+    redirect_to"/top"
+  end
+  
   def index
+    ＠lists=List.all
   end
 
   def show
@@ -10,4 +23,12 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  #ストロングパラメーター
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+
 end
+  
